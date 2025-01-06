73b66523ac6c67845a988e7b093e63cfbe10d714
Ishay Cohen <IshayDavidCohen@gmail.com>
2025-01-07 00:21:18 +0200

    Feature(api): Validation Formats
    Used for validating incoming data from frontend.
    
    Tested-by: Ishay Cohen <ishaydavidcohen@gmail.com>
    Signed-off-by: Ishay Cohen <IshayDavidCohen@gmail.com>

---
+++ app/utilities/validation_formats.py
@@ -0,0 +1,39 @@
+SUPPLIER = {
+    'bid': str,
+    'companyName': str,
+    'desc': str,
+    'icon': str,
+    'banner': str,
+    'email': str,
+    'phone': str,
+    'address': str,
+    'shippingAddress': str,
+    'categories': list
+}
+BUSINESS = {
+    'bid': str,
+    'companyName': str,
+    'desc': str,
+    'icon': str,
+    'banner': str,
+    'email': str,
+    'phone': str,
+    'address': str,
+    'shippingAddress': str,
+}
+SUPPLIER_ITEM = {
+    'supplier_id': str,
+    'itemName': str,
+    'itemCategory': str,
+    'image': str,
+    'desc': str,
+    'basePrice': float,
+    'unit': str,
+    'currency': str
+}
+HANDSHAKE = {
+    'sender_id': str,
+    'recipient_id': str,
+    'sender_type': str,
+    'recipient_type': str
+}
